48e9f44463c64b792bd5fffc86cc6d7b5331ff0d
ZhengXianda <xianda.zheng@jiamiantech.com>
2023-01-03 18:29:17 +0800

    fix publish

--- publish.sh
+++ publish.sh
@@ -3,9 +3,9 @@ NEW_VERSION=$1
 REGISTRY="tkregistry"
 
 if [ $NEW_VERSION ];then
-  vpl publish --registry=${HOME}/.vpl/registries/${REGISTRY} --update-version=$NEW_VERSION
+  vpl publish --reg=${HOME}/.vpl/registries/${REGISTRY} --update-version=$NEW_VERSION
 else
-  vpl publish --registry=${HOME}/.vpl/registries/${REGISTRY} 
+  vpl publish --reg=${HOME}/.vpl/registries/${REGISTRY} 
 fi
 
 NAME=`vpl scan name`
