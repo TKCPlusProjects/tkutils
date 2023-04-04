1c31a50369162cf7c3b831c11380b9a1aba70fb8
ZhengXianda <xianda.zheng@jiamiantech.com>
2023-04-05 02:47:26 +0800

    format

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -4,27 +4,25 @@
 using namespace std;
 using namespace tkht;
 
-bool descending(int a, int b) {
-    return a > b;
-}
+bool descending(int a, int b) { return a > b; }
 
-int main() 
-{
+int main() {
   TKArray<int> array, ret;
 
-  for (int i = 0; i < 10; i++)
-  {
+  for (int i = 0; i < 10; i++) {
     array.push_diff(i);
   }
 
   ret = array - array[0];
 
-  for (int v : ret) cout << v << ", ";
+  for (int v : ret)
+    cout << v << ", ";
   cout << endl;
 
   array.sort(descending);
 
-  for (int v : array) cout << v << ", ";
+  for (int v : array)
+    cout << v << ", ";
   cout << endl;
 
   return 1;
--- lib/include/tkutils/tkarray.hpp
+++ lib/include/tkutils/tkarray.hpp
@@ -1,285 +1,199 @@
 #ifndef tkarray_hpp
 #define tkarray_hpp
 
-#include <stdio.h>
-#include <set>
-#include <vector>
 #include <algorithm>
+#include <set>
+#include <stdio.h>
+#include <vector>
 
 using namespace std;
 
-namespace tkht
-{
+namespace tkht {
 
-    template <class T, class Alloc = allocator<T>>
-    class TKArray
-    {
+template <class T, class Alloc = allocator<T>> class TKArray {
 
-        typedef typename vector<T>::iterator iterator;
+  typedef typename vector<T>::iterator iterator;
 
-    private:
-        set<T> r;
-        vector<T> v;
+private:
+  set<T> r;
+  vector<T> v;
 
-    public:
-        TKArray()
-        {
-            TKArray({});
-        }
+public:
+  TKArray() : TKArray({}) {}
 
-        TKArray(initializer_list<T> __v)
-        {
-            v = __v;
-        }
+  TKArray(initializer_list<T> __v) { v = __v; }
 
-        template<typename _Compare>
-        TKArray(initializer_list<T> __v, _Compare __compare)
-        {
-            v = __v;
-            sort(__compare);
-        }
+  template <typename _Compare>
+  TKArray(initializer_list<T> __v, _Compare __compare) {
+    v = __v;
+    sort(__compare);
+  }
 
-        // calculate
+  // calculate
 
-        unsigned long size() const
-        {
-            return v.size();
-        }
+  unsigned long size() const { return v.size(); }
 
-        bool contain(T __v) const
-        {
-            auto that = const_cast<TKArray *>(this);
-            return that->find(__v) != that->end();
-        }
+  bool contain(T __v) const {
+    auto that = const_cast<TKArray *>(this);
+    return that->find(__v) != that->end();
+  }
 
-        T front()
-        {
-            return v.front();
-        }
+  T front() { return v.front(); }
 
-        T back()
-        {
-            return v.back();
-        }
+  T back() { return v.back(); }
 
-        // operator
+  // operator
 
-        T operator[](size_t pos)
-        {
-            return v[pos];
-        }
+  T operator[](size_t pos) { return v[pos]; }
 
-        // operator v
+  // operator v
 
-        TKArray operator+(T __v)
-        {
-            TKArray that = copy();
-            that += __v;
-            return that;
-        }
+  TKArray operator+(T __v) {
+    TKArray that = copy();
+    that += __v;
+    return that;
+  }
 
-        TKArray operator-(T __v)
-        {
-            TKArray that = copy();
-            that -= __v;
-            return that;
-        }
+  TKArray operator-(T __v) {
+    TKArray that = copy();
+    that -= __v;
+    return that;
+  }
 
-        void operator+=(T __v)
-        {
-            push(__v);
-        }
+  void operator+=(T __v) { push(__v); }
 
-        void operator-=(T __v)
-        {
-            erase(__v);
-        }
+  void operator-=(T __v) { erase(__v); }
 
-        // operator v
+  // operator v
 
-        TKArray &operator=(vector<T> __a)
-        {
-            this->v = __a;
-            return *this;
-        }
+  TKArray &operator=(vector<T> __a) {
+    this->v = __a;
+    return *this;
+  }
 
-        TKArray operator+(vector<T> __a)
-        {
-            TKArray that = copy();
-            that += __a;
-            return that;
-        }
+  TKArray operator+(vector<T> __a) {
+    TKArray that = copy();
+    that += __a;
+    return that;
+  }
 
-        TKArray operator-(vector<T> __a)
-        {
-            TKArray that = copy();
-            that -= __a;
-            return that;
-        }
+  TKArray operator-(vector<T> __a) {
+    TKArray that = copy();
+    that -= __a;
+    return that;
+  }
 
-        void operator+=(vector<T> __a)
-        {
-            for (T __v : __a)
-            {
-                push(__v);
-            }
-        }
+  void operator+=(vector<T> __a) {
+    for (T __v : __a) {
+      push(__v);
+    }
+  }
 
-        void operator-=(vector<T> __a)
-        {
-            for (T __v : __a)
-            {
-                erase(__v);
-            }
-        }
+  void operator-=(vector<T> __a) {
+    for (T __v : __a) {
+      erase(__v);
+    }
+  }
 
-        // operator a
+  // operator a
 
-        TKArray &operator=(TKArray<T> __a)
-        {
-            this->v = __a.v;
-            return *this;
-        }
+  TKArray &operator=(TKArray<T> __a) {
+    this->v = __a.v;
+    return *this;
+  }
 
-        TKArray operator+(TKArray<T> __a)
-        {
-            TKArray that = copy();
-            that += __a;
-            return that;
-        }
+  TKArray operator+(TKArray<T> __a) {
+    TKArray that = copy();
+    that += __a;
+    return that;
+  }
 
-        TKArray operator-(TKArray<T> __a)
-        {
-            TKArray that = copy();
-            that -= __a;
-            return that;
-        }
+  TKArray operator-(TKArray<T> __a) {
+    TKArray that = copy();
+    that -= __a;
+    return that;
+  }
 
-        void operator+=(TKArray<T> __a)
-        {
-            for (T __v : __a)
-            {
-                push(__v);
-            }
-        }
+  void operator+=(TKArray<T> __a) {
+    for (T __v : __a) {
+      push(__v);
+    }
+  }
 
-        void operator-=(TKArray<T> __a)
-        {
-            for (T __v : __a)
-            {
-                erase(__v);
-            }
-        }
+  void operator-=(TKArray<T> __a) {
+    for (T __v : __a) {
+      erase(__v);
+    }
+  }
 
-        // actions
+  // actions
 
-        void push(T __v)
-        {
-            v.push_back(__v);
-        }
+  void push(T __v) { v.push_back(__v); }
 
-        void push_diff(T __v)
-        {
-            if (!repeat(__v))
-            {
-                if (contain(__v))
-                {
-                    erase(__v);
-                    repeat_push(__v);
-                }
-                else
-                {
-                    push(__v);
-                }
-            }
-        }
+  void push_diff(T __v) {
+    if (!repeat(__v)) {
+      if (contain(__v)) {
+        erase(__v);
+        repeat_push(__v);
+      } else {
+        push(__v);
+      }
+    }
+  }
 
-        void push_unique(T __v)
-        {
-            if (!repeat(__v))
-            {
-                if (contain(__v))
-                {
-                    repeat_push(__v);
-                }
-                else
-                {
-                    push(__v);
-                }
-            }
-        }
+  void push_unique(T __v) {
+    if (!repeat(__v)) {
+      if (contain(__v)) {
+        repeat_push(__v);
+      } else {
+        push(__v);
+      }
+    }
+  }
 
-        void erase(T __v)
-        {
-            iterator it = find(__v);
-            bool nonexist = it == end();
-            if (!nonexist)
-            {
-                v.erase(it);
-            }
-        }
+  void erase(T __v) {
+    iterator it = find(__v);
+    bool nonexist = it == end();
+    if (!nonexist) {
+      v.erase(it);
+    }
+  }
 
-        void clear()
-        {
-            v.clear();
-        }
+  void clear() { v.clear(); }
 
-        template<typename _Compare>
-        void sort(_Compare __compare)
-        {
-            std::sort(begin(), end(), __compare);
-        }
+  template <typename _Compare> void sort(_Compare __compare) {
+    std::sort(begin(), end(), __compare);
+  }
 
-        template<typename _Compare>
-        TKArray sorted(_Compare __compare) const
-        {
-            TKArray that = copy();
-            that.sort(__compare);
-            return that;
-        }
+  template <typename _Compare> TKArray sorted(_Compare __compare) const {
+    TKArray that = copy();
+    that.sort(__compare);
+    return that;
+  }
 
-        TKArray copy() const
-        {
-            TKArray that;
-            that.v = this->v;
-            return that;
-        }
+  TKArray copy() const {
+    TKArray that;
+    that.v = this->v;
+    return that;
+  }
 
-        // iterator
+  // iterator
 
-        iterator begin()
-        {
-            return v.begin();
-        }
+  iterator begin() { return v.begin(); }
 
-        iterator end()
-        {
-            return v.end();
-        }
+  iterator end() { return v.end(); }
 
-        iterator find(T __v)
-        {
-            return std::find(begin(), end(), __v);
-        }
+  iterator find(T __v) { return std::find(begin(), end(), __v); }
 
-        // repeat
+  // repeat
 
-        bool repeat(T __v) const
-        {
-            return r.count(__v);
-        }
+  bool repeat(T __v) const { return r.count(__v); }
 
-        void repeat_push(T __v)
-        {
-            r.insert(__v);
-        }
+  void repeat_push(T __v) { r.insert(__v); }
 
-        void repeat_clear()
-        {
-            r.clear();
-        }
-    };
+  void repeat_clear() { r.clear(); }
+};
 
-}
+} // namespace tkht
 
 #endif /* tkarray_hpp */
